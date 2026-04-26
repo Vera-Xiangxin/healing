d6dfd9f6ed8f2a9e3213711ef6bb7c452ec5ebdb
Vera-Xiangxin <180817507@qq.com>
2026-04-26 09:55:46 +0800

    Add files via upload

---
+++ src/test/java/com/healing/HealingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
